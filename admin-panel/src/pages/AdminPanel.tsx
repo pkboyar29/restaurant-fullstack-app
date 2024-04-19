952e19e680047b2e189d705bded3cfe68ee6b386
Eduard Musatov <pkboyar09@gmail.com>
2024-04-19 09:41:43 +0300

    configure project configs and project structure

---
+++ admin-panel/src/pages/AdminPanel.tsx
@@ -0,0 +1,10 @@
+function AdminPanel() {
+
+   return (
+      <div className="admin-panel">
+         Административная панель
+      </div>
+   )
+}
+
+export default AdminPanel
---
+++ admin-panel/src/pages/SignIn.tsx
@@ -0,0 +1,9 @@
+function SignIn() {
+   return (
+      <>
+      Войти в систему
+      </>
+   )
+}
+
+export default SignIn
