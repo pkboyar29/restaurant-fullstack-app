4b76ec6e55e4a4169e731025969ce44141a1f417
Eduard Musatov <pkboyar09@gmail.com>
2024-05-13 18:23:56 +0300

    add adding a menu section endpoint

--- backend/src/main/java/com/example/backend/controllers/MenuSectionController.java
+++ backend/src/main/java/com/example/backend/controllers/MenuSectionController.java
@@ -5,12 +5,11 @@ import com.example.backend.services.MenuSectionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:5173")
 @RestController
@@ -28,5 +27,21 @@ public class MenuSectionController {
         List<MenuSection> menuSections = menuSectionService.getAllMenuSections();
 
         return ResponseEntity.status(HttpStatus.OK).body(menuSections);
+    }
+
+    @PostMapping
+    public ResponseEntity<Map<String, String>> addMenuSection(@RequestBody MenuSection menuSectionDTO) {
+        Map <String, String> responseBody = new HashMap<>();
+
+        try {
+            menuSectionService.addMenuSection(menuSectionDTO);
+            responseBody.put("message", "Successful");
+        }
+        catch (Exception e) {
+            responseBody.put("message", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseBody);
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseBody);
     }
 }
--- backend/src/main/java/com/example/backend/services/MenuSectionService.java
+++ backend/src/main/java/com/example/backend/services/MenuSectionService.java
@@ -17,7 +17,18 @@ public class MenuSectionService {
     }
 
     public List<MenuSection> getAllMenuSections() {
-
         return menuSectionRepository.findAll();
+    }
+
+    public void addMenuSection(MenuSection menuSectionDTO) {
+        MenuSection newMenuSection = new MenuSection();
+        newMenuSection.setName(menuSectionDTO.getName());
+        newMenuSection.setDescr(menuSectionDTO.getDescr());
+
+        try {
+            menuSectionRepository.save(newMenuSection);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to add menu section");
+        }
     }
 }
