8d8f7d2a677292e2a24c1476017d32d60331f14b
Eduard Musatov <pkboyar09@gmail.com>
2024-05-15 21:39:01 +0300

    header layout

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -1,11 +1,16 @@
 import { Routes, Route, Navigate } from "react-router-dom"
-import SignInPage from "../pages/SignInPage/SignInPage"
-import SignUpPage from "../pages/SignUpPage/SignUpPage"
+
+import SignInPage from '../pages/SignInPage/SignInPage'
+import SignUpPage from '../pages/SignUpPage/SignUpPage'
+import Header from '../components/Header/Header'
 
 function App() {
 
   return (
     <div className='App'>
+
+      <Header />
+
       <Routes>
         <Route path='/sign-in' element={<SignInPage />} />
         <Route path='/sign-up' element={<SignUpPage />} />
---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,33 @@
+import styles from './Header.module.scss'
+import { Link } from 'react-router-dom'
+
+import logo from '../../assets/logo.svg'
+import cart from '../../assets/cart.svg'
+
+function Header() {
+   return (
+      <header className={styles['header']}>
+         <div className={`${styles['container']} ${styles['header__container']}`}>
+            <img className={styles['header__img']} src={logo} alt='logo' />
+
+            <nav className={styles['header__nav']}>
+               <Link to='/' className={styles['header__item']}>Меню</Link>
+               <Link to='/' className={styles['header__item']}>Новости</Link>
+               <Link to='/' className={styles['header__item']}>О нас</Link>
+            </nav>
+
+            <div className={styles['header__right']}>
+               <div className={styles['header__cart']}>
+                  <img src={cart} alt='cart' />
+                  <div className={styles['header__cart_count']}>0</div>
+               </div>
+
+               <button className={styles['header__signin']}>Войти</button>
+            </div>
+
+         </div>
+      </header>
+   )
+}
+
+export default Header
