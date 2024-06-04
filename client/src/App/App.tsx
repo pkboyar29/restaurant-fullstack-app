18b882c0a2a079584ede9672bfe936e19faa516e
Eduard Musatov <pkboyar09@gmail.com>
2024-06-04 18:22:39 +0300

    add cutsom hook useCart

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -1,8 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom"
-import { MenuPosition } from "../ts/types/MenuPosition"
 import { Client } from '../ts/types/Client'
 import { useState, useEffect } from 'react'
-import { OrderPosition } from '../ts/types/OrderPosition'
+import useCart from '../hooks/useCart'
 
 import SignInPage from '../pages/SignInPage/SignInPage'
 import SignUpPage from '../pages/SignUpPage/SignUpPage'
@@ -12,109 +11,11 @@ import TakeawayOrderPage from "../pages/TakeawayOrderPage/TakeawayOrderPage"
 
 function App() {
 
-  const [numberCart, setNumberCart] = useState<number>(0)
   const [currentClient, setCurrentClient] = useState<Client | null>(null)
-  const [cart, setCart] = useState<OrderPosition[]>([])
-
-  const updateNumberCart = (cart: OrderPosition[]) => {
-    let totalNumberCart = 0
-    cart.map((item: OrderPosition) => {
-      totalNumberCart += item.number
-    })
-    setNumberCart(totalNumberCart)
-  }
-
-  const setCartItem = (menuPosition: MenuPosition) => {
-    const cartLocalStorage = localStorage.getItem('cart')
-
-    if (!cartLocalStorage) {
-      const newOrderPositon: OrderPosition = {
-        number: 1,
-        totalPrice: menuPosition.price,
-        menuPositon: menuPosition
-      }
-      setCart([newOrderPositon])
-      localStorage.setItem('cart', JSON.stringify([newOrderPositon]))
-
-      setNumberCart(1)
-
-    } else {
-      const cartt = JSON.parse(cartLocalStorage)
-
-      if (cartt.some((item: OrderPosition) => item.menuPositon.id === menuPosition.id)) {
-        cartt.forEach((item: OrderPosition) => {
-          if (item.menuPositon.id === menuPosition.id) {
-            item.number += 1
-            item.totalPrice += menuPosition.price
-          }
-        })
-      }
-      else {
-        const newOrderPosition: OrderPosition = {
-          number: 1,
-          totalPrice: menuPosition.price,
-          menuPositon: menuPosition
-        }
-        cartt.push(newOrderPosition)
-      }
-
-      setCart(cartt)
-      localStorage.setItem('cart', JSON.stringify(cartt))
-
-      setNumberCart(numberCart + 1)
-    }
-  }
-
-  const deleteCartItem = (menuPositionId: number) => {
-    const updatedCart = cart.filter((item: OrderPosition) => item.menuPositon.id !== menuPositionId)
-
-    setCart(updatedCart)
-    localStorage.setItem('cart', JSON.stringify(updatedCart))
-    updateNumberCart(updatedCart)
-  }
-
-  const changeNumberCartItem = (menuPositionId: number, plus: boolean) => {
-    const updatedCart: OrderPosition[] = cart.map((item: OrderPosition) => {
-      if (item.menuPositon.id === menuPositionId) {
-        let newNumber = item.number
-        let newTotalPrice = item.totalPrice
-
-        if (plus && item.number < 10) {
-          newNumber += 1
-          newTotalPrice += item.menuPositon.price
-        } else if (!plus && item.number > 1) {
-          newNumber -= 1
-          newTotalPrice -= item.menuPositon.price
-        }
-
-        return {
-          ...item,
-          number: newNumber,
-          totalPrice: newTotalPrice
-        }
-      }
-      return item
-    })
-
-    setCart(updatedCart)
-    localStorage.setItem('cart', JSON.stringify(updatedCart))
-    updateNumberCart(updatedCart)
-  }
-
-  const clearCart = () => {
-    setCart([])
-    setNumberCart(0)
-    localStorage.removeItem('cart')
-  }
+  const { numberCart, cart, setCartItem, deleteCartItem, changeNumberCartItem, clearCart, initiateCart } = useCart()
 
   useEffect(() => {
-    const cartLocalStorage = localStorage.getItem('cart')
-    if (cartLocalStorage) {
-      const cartt: OrderPosition[] = JSON.parse(cartLocalStorage)
-
-      setCart(cartt)
-      updateNumberCart(cartt)
-    }
+    initiateCart()
   }, [])
 
   const signOut = () => {
